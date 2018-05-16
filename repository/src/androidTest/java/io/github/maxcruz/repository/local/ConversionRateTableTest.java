11d0b28450c8c0f9d75bbbdbde48bfffc2ecb1c3
Max Cruz <max.raul@gmail.com>
2018-05-16 12:21:15 -0500

    Local integration tests

--- repository/src/androidTest/java/io/github/maxcruz/repository/local/ConversionRateTableTest.java
+++ repository/src/androidTest/java/io/github/maxcruz/repository/local/ConversionRateTableTest.java
@@ -12,12 +12,12 @@ import org.junit.runner.RunWith;
 
 import java.util.List;
 
-import io.github.maxcruz.repository.local.entity.ConversionRate;
+import io.github.maxcruz.repository.local.entity.ConversionRateTable;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
-public class ConversionRateTest {
+public class ConversionRateTableTest {
 
     private RatesDatabase ratesDatabase;
     private ConversionRateDao conversionRateDao;
@@ -37,7 +37,7 @@ public class ConversionRateTest {
     @Test
     public void createRateAndGetByCode() {
         // Given
-        ConversionRate rate = getSampleRate(1, "COP", 3000.0);
+        ConversionRateTable rate = getSampleRate(1, "COP", 3000.0);
 
         // When
         conversionRateDao.insertAll(rate);
@@ -50,7 +50,7 @@ public class ConversionRateTest {
     @Test
     public void createRateAndGetById() {
         // Given
-        ConversionRate rate = getSampleRate(1, "COP", 3000.0);
+        ConversionRateTable rate = getSampleRate(1, "COP", 3000.0);
 
         // When
         conversionRateDao.insertAll(rate);
@@ -62,7 +62,7 @@ public class ConversionRateTest {
     @Test
     public void deleteRateAndCount() {
         // Given
-        ConversionRate rate = getSampleRate(1, "COP", 3000.0);
+        ConversionRateTable rate = getSampleRate(1, "COP", 3000.0);
 
         // When
         conversionRateDao.insertAll(rate);
@@ -75,9 +75,9 @@ public class ConversionRateTest {
     @Test
     public void clearRateAndCount() {
         // Given
-        ConversionRate rate1 = getSampleRate(1, "COP", 3000.0);
-        ConversionRate rate2 = getSampleRate(2, "EUR", 1.5);
-        ConversionRate rate3 = getSampleRate(3, "JPY", 2.2);
+        ConversionRateTable rate1 = getSampleRate(1, "COP", 3000.0);
+        ConversionRateTable rate2 = getSampleRate(2, "EUR", 1.5);
+        ConversionRateTable rate3 = getSampleRate(3, "JPY", 2.2);
 
         // When
         conversionRateDao.insertAll(rate1);
@@ -92,27 +92,27 @@ public class ConversionRateTest {
     @Test
     public void getAllEntries() {
         // Given
-        ConversionRate rate1 = getSampleRate(1, "COP", 3000.0);
-        ConversionRate rate2 = getSampleRate(2, "EUR", 1.5);
-        ConversionRate rate3 = getSampleRate(3, "JPY", 2.2);
+        ConversionRateTable rate1 = getSampleRate(1, "COP", 3000.0);
+        ConversionRateTable rate2 = getSampleRate(2, "EUR", 1.5);
+        ConversionRateTable rate3 = getSampleRate(3, "JPY", 2.2);
 
         // When
         conversionRateDao.insertAll(rate1);
         conversionRateDao.insertAll(rate2);
         conversionRateDao.insertAll(rate3);
-        List<ConversionRate> rates = conversionRateDao.getAll();
+        List<ConversionRateTable> rates = conversionRateDao.getAll();
 
         // Then
         assertEquals(3, conversionRateDao.countAll());
         assertEquals(3, rates.size());
     }
 
-    private ConversionRate getSampleRate(int id, String code, Double rate) {
-        ConversionRate conversionRate = new ConversionRate();
-        conversionRate.setUid(id);
-        conversionRate.setCode(code);
-        conversionRate.setRate(rate);
-        return conversionRate;
+    private ConversionRateTable getSampleRate(int id, String code, Double rate) {
+        ConversionRateTable conversionRateTable = new ConversionRateTable();
+        conversionRateTable.setUid(id);
+        conversionRateTable.setCode(code);
+        conversionRateTable.setRate(rate);
+        return conversionRateTable;
     }
 
 }
