23ae4c621ed8bd01724aa5f9f67170e3f66f95b0
Max Cruz <max.raul@gmail.com>
2018-05-17 00:39:05 -0500

    ButterKnife and control animation

--- app/src/main/java/io/github/maxcruz/currencyapp/splash/SplashActivity.java
+++ app/src/main/java/io/github/maxcruz/currencyapp/splash/SplashActivity.java
@@ -1,15 +1,43 @@
 package io.github.maxcruz.currencyapp.splash;
 
+import android.animation.Animator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.maxcruz.currencyapp.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R.id.animationView)
+    protected LottieAnimationView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        controlSplashAnimation();
+    }
+
+    private void controlSplashAnimation() {
+        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) { }
+
+            @Override
+            public void onAnimationCancel(Animator animation) { }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) { }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                finish();
+            }
+        });
     }
 }
